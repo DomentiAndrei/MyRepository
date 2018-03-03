d43ef997d58f3af44e09a48b9bab29dcf37d54e9
DomentiAndrei <36136321+DomentiAndrei@users.noreply.github.com>
2018-03-03 11:27:10 +0200

    Update Parsing_with_First_JSON_Par.java

--- Automation/Parsing/JSON/Parsing_with_First_JSON_Par.java
+++ Automation/Parsing/JSON/Parsing_with_First_JSON_Par.java
@@ -10,6 +10,7 @@ import org.json.simple.JSONObject;
 
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.openqa.selenium.json.Json;
 
 public class Parsing_with_First_JSON_Par {
 
@@ -47,6 +48,32 @@ public class Parsing_with_First_JSON_Par {
 
 		JSONObject structure = (JSONObject) jsonObj.get("address");
 		System.out.println("Number of House: " + structure.get("Nr"));
+		
+		//
+		String FirstNam = (String) jsonObj.get("FirstNam");
+		System.out.println(FirstNam);
+		
+		JSONObject stru = (JSONObject) jsonObj.get("addres");
+		System.out.println("Country is: " + stru.get("Country"));
+		
+		long ag = (Long) jsonObj.get("ag");
+		System.out.println("Maria has: " + ag);
+		
+		
+		JSONArray pN = (JSONArray) jsonObj.get("PhoneNumber");
+		Iterator<String> iterato = pN.iterator();
+		while (iterato.hasNext()) {
+			System.out.println(iterato.next());
+		}
+		
+		JSONObject str = (JSONObject) jsonObj.get("addres");
+		System.out.println("Postal Code is: " + str.get("PostalCode"));
+		
+		
+		//ObjectMapper mapper = new ObjectMapper()
+		
+//		JSONObject st = (JSONObject) jsonObj.get("addres"+"Weight");
+//		System.out.println("Weight is: " + st.get("Winter"));
 		
 		
 		
